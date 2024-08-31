14761529bd6dd50242da5c24fc42b203a6adaa2a
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2024-08-31 17:40:38 +0800

    Update database

--- flask/sql/mock-db/mock-queries.sql
+++ flask/sql/mock-db/mock-queries.sql
@@ -1,2 +1,11 @@
 USE mock;
-SELECT * FROM users;
+-- SELECT * FROM users;
+
+-- SELECT * FROM invoice;
+
+SELECT *
+FROM users
+CROSS JOIN invoice
+WHERE users.name = invoice.name
+AND users.email = invoice.email
+AND users.phone = invoice.phone;
