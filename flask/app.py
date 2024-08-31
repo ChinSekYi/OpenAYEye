161fb323f367356017947ff3021f9a0efec0a90c
EkMi00 <110707998+EkMi00@users.noreply.github.com>
2024-08-31 14:25:55 +0800

    Added db init

--- flask/app.py
+++ flask/app.py
@@ -18,6 +18,39 @@ def create_app():
 
 app, mysql = create_app()
 
+with app.app_context():
+    curs = mysql.connect.cursor()
+    query_string = '''
+            DROP TABLE IF EXISTS users;
+            CREATE TABLE users (
+            id SMALLINT UNSIGNED NOT NULL UNIQUE,
+            name VARCHAR(45) NOT NULL,
+            email VARCHAR(45) NOT NULL,
+            age SMALLINT UNSIGNED NOT NULL,
+            phone CHAR(20) NOT NULL,
+            access VARCHAR(45) NOT NULL,
+            PRIMARY KEY  (id)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+            SET AUTOCOMMIT=0;
+            INSERT INTO users (id, name, email, age, phone, access) VALUES 
+            (1, "Jon Snow", "jonsnow@gmail.com", 35, "(665)121-5454", "admin"),
+            (2, "Cersei Lannister", "cerseilannister@gmail.com", 42, "(421)314-2288", "manager"),
+            (3, "Jaime Lannister", "jaimelannister@gmail.com", 45, "(422)982-6739", "user"),
+            (4, "Anya Stark", "anyastark@gmail.com", 16, "(921)425-6742", "admin"),
+            (5, "Daenerys Targaryen", "daenerystargaryen@gmail.com", 31, "(421)445-1189", "user"),
+            (6, "Ever Melisandre", "evermelisandre@gmail.com", 150, "(232)545-6483", "manager"),
+            (7, "Ferrara Clifford", "ferraraclifford@gmail.com", 44, "(543)124-0123", "user"),
+            (8, "Rossini Frances", "rossinifrances@gmail.com", 36, "(222)444-5555", "user"),
+            (9, "Harvey Roxie", "harveyroxie@gmail.com", 65, "(444)555-6239", "admin");
+
+            COMMIT;
+        '''
+    curs.execute(query_string)
+    mysql.connect.commit()
+    curs.close()
+
+
+
 @app.get("/health")
 def health():
     return {"message": "health ok"}
@@ -29,7 +62,7 @@ def home():
 # @app.get("/get_Table")
 @app.route('/getTable', methods=['GET'])
 def get_Entries(columns = ['id', 'name', 'email', 'age', 'phone', 'access'], table = 'users'):
-    curs = mysql.connection.cursor()
+    curs = mysql.connect.cursor()
     col_string = ", ".join(columns)
     curs.execute('''SELECT {} FROM {} LIMIT 10'''.format(col_string, table))
     fetched = curs.fetchall()
@@ -39,7 +72,7 @@ def get_Entries(columns = ['id', 'name', 'email', 'age', 'phone', 'access'], tab
 
 @app.get("/showTables")
 def show_Tables():
-    curs = mysql.connection.cursor()
+    curs = mysql.connect.cursor()
     curs.execute('''SHOW TABLES''')
     fetched = curs.fetchall()
     curs.close()
