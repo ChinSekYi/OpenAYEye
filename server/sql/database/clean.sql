0c1580287e403fcfa18e6b2a10b9cf843a062737
EkMi00 <five86284@gmail.com>
2024-10-26 18:11:20 +0800

    update files, README

---
+++ server/sql/database/clean.sql
@@ -0,0 +1,22 @@
+DROP SCHEMA IF EXISTS transact;
+CREATE SCHEMA transact;
+
+USE transact;
+SELECT *
+FROM users;
+
+USE transact;
+SELECT *
+FROM credit_cards;
+
+USE transact;
+SELECT *
+FROM transactions;
+
+USE transact;
+SELECT *
+FROM engagement;
+
+USE transact;
+SELECT *
+FROM santender;
