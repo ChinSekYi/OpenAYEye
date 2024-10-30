f236932508a2d1598a56c68c00fe843926a0b92b
Chin Sek Yi <sekyichin@gmail.com>
2024-10-30 14:36:26 +0800

    Update reco sys pipeline

--- src/components/Reco_sys_data_transformation.py
+++ src/components/Reco_sys_data_transformation.py
@@ -7,6 +7,7 @@ import os
 import sys
 from dataclasses import dataclass
 from pathlib import Path
+from statistics import median
 
 import numpy as np
 import pandas as pd
@@ -15,7 +16,6 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer, MinMaxScaler
 
-from notebook.data_cleaning import AsDiscrete, map_class_labels
 # Custom imports
 from src.exception import CustomException
 from src.logger import logging
@@ -88,35 +88,107 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e, sys) from e
 
-    def process_csv(csv_file_path, output_file_path, column_mapping, columns_to_drop):
-    # Read the original CSV file
-    df = pd.read_csv(csv_file_path)
+    def process_csv(self, csv_file_path, output_file_path, column_mapping, columns_to_drop):
+        # Read the original CSV file
+        df = pd.read_csv(csv_file_path)
 
-    # Drop columns if they exist in the DataFrame
-    df = df.drop(columns=[col for col in columns_to_drop if col in df.columns])
+        # Drop columns if they exist in the DataFrame
+        df = df.drop(columns=[col for col in columns_to_drop if col in df.columns])
 
-    # Rename the columns to English
-    df.rename(columns=column_mapping, inplace=True)
+        # Rename the columns to English
+        df.rename(columns=column_mapping, inplace=True)
 
-    # Save the updated DataFrame to a new CSV file
-    df.to_csv(output_file_path, index=False)
-    print(f"CSV file successfully relabelled and saved to {output_file_path}")
-    return df
+        # Save the updated DataFrame to a new CSV file
+        df.to_csv(output_file_path, index=False)
+        logging(f"CSV file successfully relabelled and saved to {output_file_path}")
+        return df
 
-    # Load column mapping from JSON
-    column_mapping = read_column_mapping("column_mapping.json")
+    def initiate_data_transformation(self, train_path, test_path):
+        """
+        Initiates the data transformation process.
+        Reads train and test datasets, applies preprocessing, and saves the preprocessor object.
 
-    # List of columns to drop if they exist
-    columns_to_drop = ['ult_fec_cli_1t', 'ind_actividad_cliente', 'cod_prov', 'conyuemp', 'tipodom']
+        Args:
+        - train_path (str): File path to the training dataset.
+        - test_path (str): File path to the testing dataset.
 
-    # Process the training and testing datasets
-    train_df = process_csv(train_file, cleaned_train_file, column_mapping, columns_to_drop)
-    test_df = process_csv(test_file, cleaned_test_file, column_mapping, columns_to_drop)
+        Returns:
+        - Tuple: Transformed train and test datasets and the file path of the preprocessor object.
+        """
+        try:
+            # Load column mapping from JSON
+            column_mapping = read_column_mapping("column_mapping.json")
 
-    # In[16]:
+            # List of columns to drop if they exist
+            columns_to_drop = ['ult_fec_cli_1t', 'ind_actividad_cliente', 'cod_prov', 'conyuemp', 'tipodom']
 
-    # Making changes to the Spanish data file to suit our needs, the simplified dataset is the one we assume is collected by companies.
-    def create_additional_columns(df):
+            # Process the training and testing datasets
+            train_df = self.process_csv(train_path, self.data_transformation_config.clean_train_file_path, column_mapping, columns_to_drop)
+            test_df = self.process_csv(test_path, self.data_transformation_config.clean_test_file_path, column_mapping, columns_to_drop)
+
+            # Making changes to the Spanish data file to suit our needs
+            train_df = self.create_additional_columns(train_df)
+            test_df = self.create_additional_columns(test_df)
+
+            # Prepare the data for machine learning
+            train_df = self.prepare_data_for_ml(train_df)
+            test_df = self.prepare_data_for_ml(test_df)
+
+            # Save preprocessor object
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_index = 
+
+            input_feature_train_df = train_df.drop(
+                train_df.columns[target_column_index], axis=1, inplace=False
+            )
+            target_feature_train_df = train_df.iloc[:, target_column_index]
+
+            input_feature_test_df = test_df.drop(
+                train_df.columns[target_column_index], axis=1, inplace=False
+            )
+            target_feature_test_df = test_df.iloc[:, target_column_index]
+
+            logging.info(
+                "Applying preprocessing object on training dataframe and testing dataframe"
+            )
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(
+                input_feature_train_df
+            )
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info("Saved preprocessing object")
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_ob_file_path,
+                obj=preprocessing_obj,
+            )
+
+            save_object(self.data_transformation_config.preprocessor_ob_file_path, preprocessing_obj)
+
+            logging.info("Data transformation completed")
+
+            logging.info("Cleaned train and test data saved to CSV files")
+            train_df.to_csv(self.data_transformation_config.clean_train_file_path, index=False)
+            test_df.to_csv(self.data_transformation_config.clean_test_file_path, index=False)
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_ob_file_path,
+            )
+
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
+    def create_additional_columns(self, df):
         # Define new column names
         fixed_deposits_col = 'fixed_deposits'
         loan_col = 'loan'
@@ -161,7 +233,7 @@ class DataTransformation:
                 "home_account",        # ind_viv_fin_ult1
             ]
             df[account_col] = df[account_columns].any(axis=1).astype(int)
-        
+
         # List of columns to drop for simplicity
         columns_to_drop = [
             'saving_account', 'guarantee', 'current_account', 'derivada_account', 'payroll_account', 
@@ -175,200 +247,56 @@ class DataTransformation:
         
         return df
 
-    # Apply to both train and test DataFrames
-    train_df = create_additional_columns(train_df)
-    test_df = create_additional_columns(test_df)
-
-    # ### Prepare the data for machine learning, all columns are converted to numerical
-
-    # In[10]:
-
     def prepare_data_for_ml(df):
+        # Specify date columns for conversion
         date_columns = ['report_date', 'contract_start_date']  # Add any other date columns if necessary
-
+        
+        # Convert date columns to datetime
         for col in date_columns:
             df[col] = pd.to_datetime(df[col], errors='coerce')
 
-        # Calculate the difference in days
+        # Calculate the difference in days for contract length
         df['contract_length'] = (df['report_date'] - df['contract_start_date']).dt.days
 
         # Insert 'contract_length' in the same spot as 'contract_start_date'
         start_date_index = df.columns.get_loc('contract_start_date')
         df.insert(start_date_index, 'contract_length', df.pop('contract_length'))
 
-        # Drop any remaining unnecessary columns or further process as needed
-        # ...
+        # Drop the original date columns and 'customer_id'
+        df = df.drop(['contract_start_date', 'report_date', 'customer_id'], axis='columns')
 
-        return df
+        # Replace missing values in 'gross_income' with the median
+        df['gross_income'] = df['gross_income'].fillna(df['gross_income'].median())
 
-    # Prepare both train and test DataFrames for ML
-    train_df = prepare_data_for_ml(train_df)
-    test_df = prepare_data_for_ml(test_df)
-    def initiate_data_transformation(self, train_path, test_path):
-        """
-        Initiates the data transformation process.
-        Reads train and test datasets, applies preprocessing, and saves the preprocessor object.
+        # Handling 'age' column
+        age_new = df['age'].replace(' NA', None).dropna().astype(int)
+        med = median(age_new)
+        df['age'] = df['age'].replace(' NA', med).astype(int)
 
-        Args:
-        - train_path (str): File path to the training dataset.
-        - test_path (str): File path to the testing dataset.
+        # Drop rows where 'country_residence' is NA
+        df = df.dropna(subset=["country_residence"])
 
-        Returns:
-        - Tuple: Transformed train and test datasets and the file path of the preprocessor object.
-        """
-        try:
-            train_df = pd.read_csv(train_path)
-            test_df = pd.read_csv(test_path)
+        # Convert 'seniority_months' to integers
+        df['seniority_months'] = df['seniority_months'].astype(int)
 
-            logging.info("Read train and test completed")
-            
-            #logging.info("Obtaining preprocessing object")
-            #preprocessing_obj = self.get_data_transformer_object()
+        # Drop 'employee_index' column
+        df = df.drop('employee_index', axis='columns')
 
-            logging.info("Drop columns")
-            columns_to_drop = ['ult_fec_cli_1t', 'ind_actividad_cliente', 'cod_prov', 'conyuemp', 'tipodom']
-            train_df.drop(columns=[col for col in columns_to_drop if col in train_df.columns], inplace=True)
-            test_df.drop(columns=[col for col in columns_to_drop if col in test_df.columns], inplace=True)
+        # One-hot encode categorical variables
+        df = pd.get_dummies(df, columns=['customer_segment', 'province_name', 'join_channel', 'country_residence'], dtype=int)
+        df = pd.get_dummies(df, columns=['deceased_index', 'foreigner_index', 'residence_index', 'customer_relation_type', 'gender', 'new_customer_index'], drop_first=True, dtype=int)
 
-            logging.info("Rename columns")
-            column_mapping = read_column_mapping("column_mapping.json")
-            train_df.rename(columns=column_mapping)
-            test_df.rename(columns=column_mapping)
+        # Reorder columns if needed (maintain original logic)
+        cols = df.columns.tolist()
+        cols = cols[:5] + cols[9:] + cols[5:9]  # Adjust indexing as needed
+        df = df[cols]
 
-            logging.info("Group products into 4 main groups")
-            # Fixed deposits
-            if 'fixed_deposits' not in train_df.columns:
-                train_df['fixed_deposits'] = train_df[
-                    ["short_term_deposits", "medium_term_deposits", "long_term_deposits"]
-                ].any(axis=1).astype(int)
+        # Move 'account' column to the last position
+        account_column = df.pop('account')
+        df['account'] = account_column
 
-            if 'fixed_deposits' not in test_df.columns:
-                test_df['fixed_deposits'] = test_df[
-                    ["short_term_deposits", "medium_term_deposits", "long_term_deposits"]
-                ].any(axis=1).astype(int)
-
-            # Loans
-            if 'loan' not in train_df.columns:
-                train_df['loan'] = train_df[["loans", "pensions"]].any(axis=1).astype(int)
-
-            if 'loan' not in test_df.columns:
-                test_df['loan'] = test_df[["loans", "pensions"]].any(axis=1).astype(int)
-
-            # Credit/debit cards
-            if 'credit_card_debit_card' not in train_df.columns:
-                train_df['credit_card_debit_card'] = train_df[["credit_card", "direct_debit"]].any(axis=1).astype(int)
-
-            if 'credit_card_debit_card' not in test_df.columns:
-                test_df['credit_card_debit_card'] = test_df[["credit_card", "direct_debit"]].any(axis=1).astype(int)
-
-            # Accounts
-            if 'account' not in train_df.columns:
-                train_df['account'] = train_df[
-                    [
-                        "saving_account", "current_account", "derivada_account",
-                        "payroll_account", "junior_account", "more_particular_account",
-                        "particular_account", "particular_plus_account", "e_account", "funds", "home_account"
-                    ]
-                ].any(axis=1).astype(int)
-
-            if 'account' not in test_df.columns:
-                test_df['account'] = test_df[
-                    [
-                        "saving_account", "current_account", "derivada_account",
-                        "payroll_account", "junior_account", "more_particular_account",
-                        "particular_account", "particular_plus_account", "e_account", "funds", "home_account"
-                    ]
-                ].any(axis=1).astype(int)
-
-            # List of columns to drop for simplicity
-            columns_to_drop = [
-                'saving_account', 'guarantee', 'current_account', 'derivada_account', 'payroll_account', 
-                'junior_account', 'more_particular_account', 'particular_account', 'particular_plus_account', 
-                'short_term_deposits', 'medium_term_deposits', 'long_term_deposits', 'e_account', 'funds', 
-                'mortgage', 'pensions', 'loans', 'taxes', 'credit_card', 'securities', 'home_account', 
-                'payroll', 'pensions_payments', 'direct_debit'
-            ]
-
-            # Drop the original columns used to create new features
-            train_df.drop(columns=[
-                "saving_account", "current_account", "derivada_account", "payroll_account",
-                "junior_account", "more_particular_account", "particular_account",
-                "particular_plus_account", "short_term_deposits", "medium_term_deposits",
-                "long_term_deposits", "e_account", "funds", "mortgage", "pensions", "loans",
-                "taxes", "credit_card", "direct_debit"
-            ], inplace=True)
-
-            test_df.drop(columns=[
-            "saving_account", "current_account", "derivada_account", "payroll_account",
-            "junior_account", "more_particular_account", "particular_account",
-            "particular_plus_account", "short_term_deposits", "medium_term_deposits",
-            "long_term_deposits", "e_account", "funds", "mortgage", "pensions", "loans",
-            "taxes", "credit_card", "direct_debit"
-            ], inplace=True)
+        return df  # Return the updated DataFrame
 
 
 
-
-
-
-
-
-
-
-            logging.info("Cleaned train and test data saved to CSV files")
-            train_df.to_csv(self.data_transformation_config.clean_train_file_path, index=False)
-            test_df.to_csv(self.data_transformation_config.clean_test_file_path, index=False)
-
-            # Save the cleaned DataFrames to CSV files (optional)
-
-
-
-
-
-
-
-
-            input_feature_train_df = train_df.drop(
-                train_df.columns[target_column_index], axis=1, inplace=False
-            )
-            target_feature_train_df = train_df.iloc[:, target_column_index]
-
-            input_feature_test_df = test_df.drop(
-                train_df.columns[target_column_index], axis=1, inplace=False
-            )
-            target_feature_test_df = test_df.iloc[:, target_column_index]
-
-            
-            
-            
-            
-            
-            logging.info(
-                "Applying preprocessing object on training dataframe and testing dataframe"
-            )
-
-            input_feature_train_arr = preprocessing_obj.fit_transform(
-                input_feature_train_df
-            )
-            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
-
-            train_arr = np.c_[
-                input_feature_train_arr, np.array(target_feature_train_df)
-            ]
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
-
-            logging.info("Saved preprocessing object")
-
-            save_object(
-                file_path=self.data_transformation_config.preprocessor_ob_file_path,
-                obj=preprocessing_obj,
-            )
-
-            return (
-                train_arr,
-                test_arr,
-                self.data_transformation_config.preprocessor_ob_file_path,
-            )
-
-        except Exception as e:
-            raise CustomException(e, sys) from e
+        
