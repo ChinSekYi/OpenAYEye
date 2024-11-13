c268fc3955af898eba6b3cca08380d01772e0c66
EkMi00 <five86284@gmail.com>
2024-11-13 23:28:34 +0800

    add again

--- server/src/main.py
+++ server/src/main.py
@@ -24,6 +24,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# print(explained_dct)
 
 def create_app(
     app=FastAPI(),
